5314f7b0b88e43349b34a6654132dfac8b74be45
Stephen Swanson <swanson.stephen.h@gmail.com>
2024-10-02 18:21:13 +0300

    reverting some changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,12 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dtt9tzpip",
   },
-  webpack(config, { isServer, dev }) {
+  webpack(config) {
+    // Add a rule to handle .svg files as React components
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
-    if (dev && !isServer) {
-      config.devtool = "inline-source-map";
-    }
-
-    // Enable source maps in production
-    if (!dev) {
-      config.devtool = "source-map";
-    }
 
     return config;
   },
